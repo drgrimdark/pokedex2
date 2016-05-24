6f3076b187b1e260c04467231b941ce7cdc7f288
patrick lujan <patrick.lujan@gmail.com>
2016-05-23 22:28:33 -0700

    First Commit

---
+++ pokedex-by-me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-me
+//
+//  Created by Mike on 5/23/16.
+//  Copyright © 2016 Little Lujan LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
