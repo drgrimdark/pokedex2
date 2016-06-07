b0793762605782a5fba22114aac89593f2e572e3
patrick lujan <patrick.lujan@gmail.com>
2016-06-06 17:27:33 -0700

    finial draft
    90% working, need to add ability to handle pokemon that evolve via
    trade and via items or held items.

---
+++ pokedex-by-me/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex-by-me
+//
+//  Created by Mike on 5/30/16.
+//  Copyright © 2016 Little Lujan LLC. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
